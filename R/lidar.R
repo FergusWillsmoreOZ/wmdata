6aba610ee7902262f90e2273e143ac0357df049c
Fergus Willsmore <Fergus.Willsmore@ozminerals.com>
2022-05-31 08:38:39 +0930

    update
    changes to lidar and metmast

--- R/lidar.R
+++ R/lidar.R
@@ -2,10 +2,5 @@
 #'
 #' A dataset containing observations recorded from the on-site LiDAR every minute.
 #'
-#' @format A data frame with 105 variables
-#' \describe{
-#'   \item{datetime}{timestamp of the observation}
-#'   ...
-#' }
 #'
 "lidar"
